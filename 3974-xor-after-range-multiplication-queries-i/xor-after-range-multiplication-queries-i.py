e5f891bd02ad00e288192fc77606d132c2daf1ec
ticiqyu <malov.i.m@mail.ru>
2026-08-30 21:41:06 +0300

    Time: 1712 ms (29.39%) | Memory: 20.2 MB (16.55%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.py
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.py
@@ -1,11 +1,11 @@
 class Solution:
     def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
         result = 0
-        for i in range(len(queries)):
-            idx = queries[i][0]
-            while idx <= queries[i][1]:
-                nums[idx] = (nums[idx] * queries[i][3]) % (10**9 + 7)
-                idx += queries[i][2]
+        for l,r,k,v in queries:
+            idx = l
+            while idx <= r:
+                nums[idx] = (nums[idx] * v) % (10**9 + 7)
+                idx += k
         print(nums)
         for num in nums:
             result ^= num
